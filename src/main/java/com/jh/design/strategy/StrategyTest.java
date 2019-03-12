c4f43579fb8f0be6e67c6793ccfcf982c574ecc3
lishijia <shijia.li@hotmail.com>
2019-03-12 13:48:37 +0800

    即spring的通过url寻找对应的Controller即可以称作为是策略模式的一种实现

--- src/main/java/com/jh/design/strategy/StrategyTest.java
+++ src/main/java/com/jh/design/strategy/StrategyTest.java
@@ -2,6 +2,9 @@ package com.jh.design.strategy;
 
 /**
  * 策略模式
+ * pring MVC 框架：MVC 框架是一个全功能的构建 Web 应用程序的 MVC 实现。
+ * 通过策略接口，MVC 框架变成为高度可配置的，MVC 容纳了大量视图技术，其中包括 JSP、Velocity、Tiles、iText 和 POI。
+ * 即spring的通过url寻找对应的Controller即可以称作为是策略模式的一种实现
  */
 public class StrategyTest {
 }
