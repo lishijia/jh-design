565367ec9823b837728d9fa9e32d2916f1c8bc75
lishijia <shijia.li@hotmail.com>
2019-03-18 18:02:38 +0800

    Decorator装饰模式

---
+++ src/main/java/com/jh/design/decorator/DecoratorTest.java
@@ -0,0 +1,10 @@
+package com.jh.design.decorator;
+
+/**
+ * @program:
+ * @description: 装饰模式 Wrapper,Decorator (结构与委托模式类同)
+ * @author: lishijia
+ * @create: 2019-03-18 17:54
+ **/
+public class DecoratorTest {
+}
