12a5a5cce4306058c6973a926b5e356b60b38ca3
lishijia <shijia.li@hotmail.com>
2019-07-31 18:39:07 +0800

    ByteBuf uml

---
+++ src/main/java/com/jh/base/BaseTest.java
@@ -0,0 +1,18 @@
+package com.jh.base;
+
+/**
+ * @program:
+ * @description:
+ * @author: lishijia
+ * @create: 2019-07-31 9:45
+ **/
+public class BaseTest {
+
+    public static void main(String[] args){
+
+        System.out.println(3 | 9);
+        System.out.println(4 & 9);
+    }
+
+
+}
